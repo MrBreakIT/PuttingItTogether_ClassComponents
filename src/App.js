1669f2d2640270de17182de5737a37f17e667657
MrBreakIT <johnpike1022@gmail.com>
2020-09-04 09:45:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,30 +16,38 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <PersonCard
-            lastName="Doe"
-            firstName="Jane"
-            age={45}
-            hairColor="Black"
-          />
-          <PersonCard
-            lastName="Smith"
-            firstName="John"
-            age={88}
-            hairColor="Brown"
-          />
-          <PersonCard
-            lastName="Fillmore"
-            firstName="Millard"
-            age={50}
-            hairColor="Brown"
-          />
-          <PersonCard
-            lastName="Smith"
-            firstName="Maria"
-            age={62}
-            hairColor="Brown"
-          />
+          <div>
+            <PersonCard
+              lastName="Doe"
+              firstName="Jane"
+              age={45}
+              hairColor="Black"
+            />
+          </div>
+          <div>
+            <PersonCard
+              lastName="Smith"
+              firstName="John"
+              age={88}
+              hairColor="Brown"
+            />
+          </div>
+          {/* <div>
+            <PersonCard
+              lastName="Fillmore"
+              firstName="Millard"
+              age={50}
+              hairColor="Brown"
+            />
+          </div>
+          <div>
+            <PersonCard
+              lastName="Smith"
+              firstName="Maria"
+              age={62}
+              hairColor="Brown"
+            />
+          </div> */}
         </header>
       </div>
     )
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        }
+        // this.increaseAge = this.increaseAge.bind(this); //this binds "this" to the method
+    }
+
+    // increaseAge() {
+    //     this.setState(prevState => {
+    //         // { age += 1 };
+    //         return { age: age += 1 }
+    //     })
+    // }
+
     render() {
         const { lastName, firstName, age, hairColor } = this.props;
         return (
-            <div className="">
-                <h2 className="">
-                    {lastName}, {firstName}
-                </h2>
-                <p className="">Age: {age}</p>
-                <p className="">Hair Color: {hairColor}</p>
+            <div className="App">
+                <div>
+                    <h2>
+                        {lastName}, {firstName}
+                    </h2>
+
+                    <p>Age: {this.state.age}</p>
+                    <p>Hair Color: {hairColor}</p>
+                    <button onClick={() => (this.setState({ age: this.state.age + 1 })) }> Add years to {firstName} {lastName}'s life.</button>
+                    <div>
+                    <button onClick={() => (this.setState({ age: this.state.age -1 })) }> Take away years for {firstName} {lastName}'s life.</button>
+                    </div>
+            </div>
             </div >
         );
     }
